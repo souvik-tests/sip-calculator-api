a2dc27d1e5eaf1b6b83d5698393c65f0423035f0
souvik-tests <souvik71200@gmail.com>
2021-11-15 01:18:59 +0530

    display status & check empty

--- index.php
+++ index.php
@@ -1,49 +1,64 @@
 <?php
+    error_reporting(0);
     if($_GET){
-        $interest = $_GET['interest'];
+        $invest = $_GET['invest'];
         $return = $_GET['return'];
         $year = $_GET['year'];
         
+        if($invest == ""){
+            $msg = array("status"=>"failed", "message"=>"no 'invest' value found");
+            echo json_encode($msg);
+        }
+        elseif($return == "") {
+            $msg = array("status"=>"failed", "message"=>"no 'return' value found");
+            echo json_encode($msg);
+        }
+        elseif($year == "") {
+            $msg = array("status"=>"failed", "message"=>"no 'year' value found");
+            echo json_encode($msg);
+        }
+        else {
+            // calculations starts - SIP
+            // storing values
+            $p = $invest;
+            $yr = $year;
+            $n = $yr * 12;
+            $r = $return;
+            $i = $r / 100 / 12;
+
+            // let's calculate
+            $abc = $i + 1;
+            $bcd = pow($abc, $n);
+            $cde = $bcd - 1;
+            $def = $abc / $i;
+            $efg = $p * $cde;
+
+            // the result
+            $fv = $efg * $def;
+            $inv = $p * $n;
+            $rtn = $fv - $inv;
+
+            // round the values
+            $fvFx = round($fv);
+            $invFx = round($inv);
+            $rtnFx = round($rtn);
+
+            // number format
+            $fvFinal = number_format($fvFx);
+            $invFinal = number_format($invFx);
+            $rtnFinal = number_format($rtnFx);
+
+            // storing result in array
+            $final_result = array("status"=>"success", "Total value"=>$fvFinal, "Invested amount"=>$invFinal, "Est. returns"=>$rtnFinal, "message"=>"value calculated sucessfully");
+
+            // printing as JSON
+            echo json_encode($final_result);
+        }
         
-        
-        // calculations starts - SIP
-        // storing values
-        $p = $interest;
-        $yr = $year;
-        $n = $yr * 12;
-        $r = $return;
-        $i = $r / 100 / 12;
-        
-        // let's calculate
-        $abc = $i + 1;
-        $bcd = pow($abc, $n);
-        $cde = $bcd - 1;
-        $def = $abc / $i;
-        $efg = $p * $cde;
-        
-        // the result
-        $fv = $efg * $def;
-        $inv = $p * $n;
-        $rtn = $fv - $inv;
-        
-        // round the values
-        $fvFx = round($fv);
-        $invFx = round($inv);
-        $rtnFx = round($rtn);
-        
-        // number format
-        $fvFinal = number_format($fvFx);
-        $invFinal = number_format($invFx);
-        $rtnFinal = number_format($rtnFx);
-        
-        // storing result in array
-        $final_result = array("Total value"=>$fvFinal, "Invested amount"=>$invFinal, "Est. returns"=>$rtnFinal);
-        
-        // printing as JSON
-        echo json_encode($final_result);
         
     }
     else {
-        echo "no";
+        $msg = array("status"=>"failed", "message"=>"no inputs found");
+        echo json_encode($msg);
     }
 ?>
