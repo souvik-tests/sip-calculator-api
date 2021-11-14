26e50a23208b07afe0196c9878265b65bf539345
souvik-tests <souvik71200@gmail.com>
2021-11-14 19:01:52 +0530

    working (alpha)

--- index.php
+++ index.php
@@ -1,10 +1,44 @@
 <?php
     if($_GET){
-        $val1 = $_GET['val1'];
-        $val2 = $_GET['val2'];
-        $val3 = $_GET['val3'];
+        $interest = $_GET['interest'];
+        $return = $_GET['return'];
+        $year = $_GET['year'];
         
-        echo $val1 . $val2 . $val3;
+        
+        
+        // calculations starts - SIP
+        // storing values
+        $p = $interest;
+        $yr = $year;
+        $n = $yr * 12;
+        $r = $return;
+        $i = $r / 100 / 12;
+        
+        // let's calculate
+        $abc = $i + 1;
+        $bcd = pow($abc, $n);
+        $cde = $bcd - 1;
+        $def = $abc / $i;
+        $efg = $p * $cde;
+        
+        // the result
+        $fv = $efg * $def;
+        $inv = $p * $n;
+        $rtn = $fv - $inv;
+        
+        // round the values
+        $fvFx = round($fv);
+        $invFx = round($inv);
+        $rtnFx = round($rtn);
+        
+        // number format
+        $fvFinal = number_format($fvFx);
+        $invFinal = number_format($invFx);
+        $rtnFinal = number_format($rtnFx);
+        
+        
+        echo $fvFinal .'<br>'. $invFinal .'<br>'. $rtnFinal;
+        
     }
     else {
         echo "no";
